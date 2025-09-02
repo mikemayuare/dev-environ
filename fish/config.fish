b901262d55dc31bde2908fe26679c3492eaf2a01
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2025-09-02 22:04:22 +0200

    added fish config

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# point to where you installed nvm (adjust if different)
+set -gx NVM_DIR $HOME/.nvm
+
+function nvm
+    bass source $NVM_DIR/nvm.sh --no-use \; nvm $argv
+end
+
+oh-my-posh init fish --config ohmyposh_themes/atomic-dracula.omp.json | source
+
+# Load nvm and use default node version
+bass source $NVM_DIR/nvm.sh --no-use ';' nvm use default > /dev/null
