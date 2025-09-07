4000274c158418b9837ede4f0a3c6b499ae0dcad
Miguelangel Leon <migueleonm@gmail.com>
2025-09-07 21:43:25 +0200

    updated sqlfluff config to format

--- nvim/.config/nvim/lua/plugins/sql-lsp.lua
+++ nvim/.config/nvim/lua/plugins/sql-lsp.lua
@@ -1,20 +1,26 @@
 return {
-  -- Formatting with sqlfluff
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        sql = { "sqlfluff" },
-      },
-    },
-  },
-
   -- Linting with sqlfluff
   {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
         sql = { "sqlfluff" },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sqlfluff_fix" },
+      },
+      formatters = {
+        sqlfluff_fix = {
+          command = "sqlfluff",
+          args = { "fix", "--dialect=bigquery", "-" },
+          stdin = true,
+        },
       },
     },
   },
