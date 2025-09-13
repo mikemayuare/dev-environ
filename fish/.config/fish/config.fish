18662f2211a8d36788f005320489e8808f9aaeb4
Miguelangel Leon <migueleonm@gmail.com>
2025-09-14 00:12:46 +0200

    set fish to catppuccine

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,10 +9,12 @@ function nvm
     bass source $NVM_DIR/nvm.sh --no-use \; nvm $argv
 end
 
-oh-my-posh init fish --config ~/.config/fish/themes/dracula-minimal.omp.json | source
+oh-my-posh init fish --config ~/.config/fish/themes/catppuccin-frappe-minimal.omp.json | source
 
 # Load nvm and use default node version
-bass source $NVM_DIR/nvm.sh --no-use ';' nvm use default > /dev/null
+bass source $NVM_DIR/nvm.sh --no-use ';' nvm use default >/dev/null
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/migue/google-cloud-sdk/path.fish.inc' ]; . '/home/migue/google-cloud-sdk/path.fish.inc'; end
+if [ -f '/home/migue/google-cloud-sdk/path.fish.inc' ]
+    . '/home/migue/google-cloud-sdk/path.fish.inc'
+end
