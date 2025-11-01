6f205ff033494bea37a896b2abad4c1392266da6
Miguelangel Leon <mike.mayuare@gmail.com>
2025-11-01 22:27:19 +0100

    minor config on waybar

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,12 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# starship integration
 starship init fish | source
-# oh-my-posh init fish --config ~/.config/fish/themes/catppuccin-frappe-minimal.omp.json | source
 
+# zoxide integration
 zoxide init fish --cmd cd | source
+
+# fzf integration
+fzf --fish | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/migue/google-cloud-sdk/path.fish.inc' ]
     . '/home/migue/google-cloud-sdk/path.fish.inc'
 end
+
+# aliases
+alias nvs='nvim $(fzf --preview="bat --color=always {}")'
