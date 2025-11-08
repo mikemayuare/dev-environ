5dda38873dac8ca09588680dcde63cb5bcbd3ac5
Miguelangel Leon <mike.mayuare@gmail.com>
2025-11-08 15:30:57 +0100

    leave only snacks picker

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -5,9 +5,6 @@ return {
     "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua", -- optional
-    "nvim-mini/mini.pick", -- optional
     "folke/snacks.nvim", -- optional
   },
 }
