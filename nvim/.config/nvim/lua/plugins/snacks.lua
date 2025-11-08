4857b52f4c010d4a5789c5fe6326b1199f12c145
Miguelangel Leon <mike.mayuare@gmail.com>
2025-11-08 19:30:15 +0100

    added snacks.gh

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
         return entry.name ~= ".DS_Store"
       end,
     },
+    gh = {},
     picker = {
       -- Enable hidden files globally for picker
       hidden = true,
@@ -18,6 +19,7 @@ return {
             },
           },
         },
+        gh_pr = {},
         files = {
           hidden = true,
         },
@@ -28,6 +30,36 @@ return {
         wo = {
           cursorline = true,
         },
+      },
+    },
+    keys = {
+      {
+        "<leader>gi",
+        function()
+          Snacks.picker.gh_issue()
+        end,
+        desc = "GitHub Issues (open)",
+      },
+      {
+        "<leader>gI",
+        function()
+          Snacks.picker.gh_issue({ state = "all" })
+        end,
+        desc = "GitHub Issues (all)",
+      },
+      {
+        "<leader>gp",
+        function()
+          Snacks.picker.gh_pr()
+        end,
+        desc = "GitHub Pull Requests (open)",
+      },
+      {
+        "<leader>gP",
+        function()
+          Snacks.picker.gh_pr({ state = "all" })
+        end,
+        desc = "GitHub Pull Requests (all)",
       },
     },
   },
