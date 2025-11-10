5b4ac6240a6f61ddf92e1d10519653e84eadedce
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2025-11-10 09:30:27 +0100

    merged nvim from master

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
         return entry.name ~= ".DS_Store"
       end,
     },
+    gh = {},
     picker = {
       -- Enable hidden files globally for picker
       hidden = true,
@@ -18,6 +19,7 @@ return {
             },
           },
         },
+        gh_pr = {},
         files = {
           hidden = true,
         },
@@ -30,10 +32,40 @@ return {
         },
       },
     },
+    keys = {
+      {
+        "<leader>gi",
+        function()
+          Snacks.picker.gh_issue()
+        end,
+        desc = "GitHub Issues (open)",
+      },
+      {
+        "<leader>gI",
+        function()
+          Snacks.picker.gh_issue({ state = "all" })
+        end,
+        desc = "GitHub Issues (all)",
+      },
+      {
+        "<leader>gp",
+        function()
+          Snacks.picker.gh_pr()
+        end,
+        desc = "GitHub Pull Requests (open)",
+      },
+      {
+        "<leader>gP",
+        function()
+          Snacks.picker.gh_pr({ state = "all" })
+        end,
+        desc = "GitHub Pull Requests (all)",
+      },
+    },
   },
   config = function(_, opts)
     require("snacks").setup(opts)
-    
+
     -- Set highlights after Snacks is loaded
     vim.api.nvim_set_hl(0, "SnacksPickerDir", { fg = "#88C0D0" })
     vim.api.nvim_set_hl(0, "Directory", { fg = "#88C0D0", bold = true })
