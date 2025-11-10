868b2df062b3f67f181a07ef34bb3a3d14176ccc
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2025-11-10 09:28:47 +0100

    added eza, fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,12 +9,24 @@ function nvm
     bass source $NVM_DIR/nvm.sh --no-use \; nvm $argv
 end
 
-oh-my-posh init fish --config ~/.config/fish/themes/catppuccin-frappe-minimal.omp.json | source
-
 # Load nvm and use default node version
 bass source $NVM_DIR/nvm.sh --no-use ';' nvm use default >/dev/null
+
+# fzf integration
+fzf --fish | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/migue/google-cloud-sdk/path.fish.inc' ]
     . '/home/migue/google-cloud-sdk/path.fish.inc'
+end
+
+# aliases
+alias nvs='nvim $(fzf --preview="bat --color=always {}")'
+
+# eza
+if command -v eza &>/dev/null
+    alias ls='eza -lh --group-directories-first --icons=auto'
+    alias lsa='ls -a'
+    alias lt='eza --tree --level=2 --long --icons --git'
+    alias lta='lt -a'
 end
