5051431a25768aa0af47ffbf6b0e52e46416189b
Miguelangel Leon <migueleonm@gmail.com>
2025-12-17 13:03:31 +0100

    switched copilot to inline

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -2,9 +2,9 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      preset = "default",
-      ["<Tab>"] = { "accept", "fallback" },
-      ["<CR>"] = { "fallback" }, -- make Enter insert newline normally
+      -- preset = "default",
+      -- ["<Tab>"] = { "accept", "fallback" },
+      -- ["<CR>"] = { "fallback" }, -- make Enter insert newline normally
     },
   },
 }
