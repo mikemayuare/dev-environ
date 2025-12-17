e4c0b1c5e5063c03682b8cbe957a46776343e1bc
Miguelangel Leon <migueleonm@gmail.com>
2025-12-17 13:07:12 +0100

    removed the autocomment sign

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("oil").setup({ default_file_explorer = true, view_options = { show_hidden = true } })
+vim.opt.formatoptions:remove({ "c", "r", "o" })
