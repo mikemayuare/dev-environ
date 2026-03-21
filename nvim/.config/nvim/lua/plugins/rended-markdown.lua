68f98abec27c3df3f27975582c02f7e2f399968b
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2026-03-21 16:50:41 +0100

    modified to default layout

---
+++ nvim/.config/nvim/lua/plugins/rended-markdown.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = true,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        enabled = true,
+        render_modes = false,
+        atx = true,
+        setext = true,
+        sign = true,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        position = "overlay",
+        signs = { "󰫎 " },
+        width = "full",
+        left_margin = 0,
+        left_pad = 0,
+        right_pad = 0,
+        min_width = 0,
+        border = false,
+        border_virtual = false,
+        border_prefix = false,
+        above = "▄",
+        below = "▀",
+        backgrounds = {
+          "RenderMarkdownH1Bg",
+          "RenderMarkdownH2Bg",
+          "RenderMarkdownH3Bg",
+          "RenderMarkdownH4Bg",
+          "RenderMarkdownH5Bg",
+          "RenderMarkdownH6Bg",
+        },
+        foregrounds = {
+          "RenderMarkdownH1",
+          "RenderMarkdownH2",
+          "RenderMarkdownH3",
+          "RenderMarkdownH4",
+          "RenderMarkdownH5",
+          "RenderMarkdownH6",
+        },
+        custom = {},
+      },
+      checkbox = {
+        enabled = true,
+      },
+      latex = {
+        enabled = true,
+        converter = "latex2text", -- This matches your checkhealth
+        highlight = "RenderMarkdownMath",
+        top_pad = 1,
+        bottom_pad = 1,
+        position = "overlay",
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = require("render-markdown").get,
+        set = require("render-markdown").set,
+      }):map("<leader>um")
+    end,
+  },
+}
