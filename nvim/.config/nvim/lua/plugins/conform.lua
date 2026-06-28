00f89cdd64aa3dc5b461957113814c5f0647a8b0
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2026-06-28 14:31:01 +0200

    python lsp on quarto files

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+  { -- Formatter plugin
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "[F]ormat buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_format", "ruff_organize_imports" },
+        -- The "injected" formatter tells conform to use treesitter to find
+        -- code blocks (like Python) and run their native formatters on them.
+        quarto = { "injected" },
+        markdown = { "injected" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
+}
