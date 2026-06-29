d527be76e1c5b7d6d384feb1601628ef7de396b3
Miguelangel Leon Mayuare <migueleonm@gmail.com>
2026-06-29 13:38:06 +0200

    use retro

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,17 @@
 return {
-	{ "neanias/everforest-nvim" },
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "everforest",
-		},
-	},
+  {
+    "alexzeitler/retro-82.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      terminal_colors = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "retro-82",
+    },
+  },
 }
